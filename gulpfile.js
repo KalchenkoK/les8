b60673149f4f85d33b297deac02ed1c37dc02c4a
KalchenkoK <7584663@gmail.com>
2021-06-26 23:29:41 +0300

    2606

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,26 @@ const gulp = require('gulp'),
       //fileinclude = require('gulp-file-include'),
       browserSync = require('browser-sync');
 
-gulp.task('sass', function(){
-    return gulp.src("./src/assets/scss/**/*.scss")
-        .pipe(sass())
+//gulp.task('sass', function(){
+   // return gulp.src("./src/assets/scss/**/*.scss")
+     /*   .pipe(sass())
         .pipe(gulp.dest('./dist/css'))
         .pipe(browserSync.stream());
-});
+});*/
 
-gulp.task('fileinclude', function() {
+/*gulp.task('fileinclude', function() {
     return gulp.src('./src/pages/index.html')
         .pipe(fileinclude({
             prefix: '@@',
             basepath: '@file'
         }))
         .pipe(gulp.dest('./'));
-});
+});*/
 
 gulp.task("watch", function(done) {
     browserSync.init({
         server:'.'
-    })
+    });
 
     //gulp.watch("./src/assets/scss/**/*.scss", gulp.series('sass'));
     //gulp.watch("./src/pages/*.html").on('change', gulp.series('fileinclude'));
